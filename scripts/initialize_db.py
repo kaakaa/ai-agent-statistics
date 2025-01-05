94c6f0bd481a49827430e83dd1cef33806208d61
kaakaa <stooner.hoe@gmail.com>
2025-01-05 23:32:18 +0900

    chore: add debug lines

--- scripts/initialize_db.py
+++ scripts/initialize_db.py
@@ -3,6 +3,9 @@ import duckdb
 
 conn = duckdb.connect('store.db')
 
+print('Initialize metadata table')
 conn.execute(Path('sql/initialize_metadata.sql').read_text())
+print('Initialize pull_request table')
 conn.execute(Path('sql/initialize_pull_request.sql').read_text())
+print('Initialize repository table')
 conn.execute(Path('sql/initialize_repository.sql').read_text())
