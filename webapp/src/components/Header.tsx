32be785178330e510664a04d1e8bbe8204a946ba
Yusuke Nemoto <kaakaa@users.noreply.github.com>
2025-01-05 12:25:58 +0900

    Add a new `Header` component with a navigation bar
    * Import `Link` from `react-router` instead of `react-router-dom`
    * Include a GitHub icon in the navigation bar

--- webapp/src/components/Header.tsx
+++ webapp/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Header: React.FC = () => {
