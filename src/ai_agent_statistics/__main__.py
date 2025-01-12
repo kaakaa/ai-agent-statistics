2c5fcc4a96bd3c3679bd6a91961ab63f74f0bfc8
kaakaa <stooner.hoe@gmail.com>
2025-01-12 13:54:50 +0900

    chore: add comments about variants for gru-agent

--- src/ai_agent_statistics/__main__.py
+++ src/ai_agent_statistics/__main__.py
@@ -28,6 +28,7 @@ SWE_AI_AGENTS = [
         "url": "https://sweep.dev/"
     },
     {
+        # For now, "gru-agent-dev[bot]" and "gru-agent-staging[bot]" are not included in the list
         "name": "gru-agent[bot]",
         "url": "https://gru.ai/"
     },
